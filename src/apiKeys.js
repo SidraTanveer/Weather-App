f78ffeb17d42e400b5de590b673ce01bcdddf78f
SidraTanveer <143613289+SidraTanveer@users.noreply.github.com>
2024-09-28 23:27:12 +0500

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5fa28d7b39235a67ee0826bfab5efc08",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
